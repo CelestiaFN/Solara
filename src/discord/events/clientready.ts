1d9d6a0d3ad75a290a6ed5f54b57ea3f3d5c69d1
Itztiva <tiva@itztiva.com>
2024-07-25 22:24:03 +0000

    wow im watching you

--- src/discord/events/clientready.ts
+++ src/discord/events/clientready.ts
@@ -1,4 +1,4 @@
-import { ActivityType, type Client } from "discord.js";
+import { ActivityType, Client } from "discord.js";
 import logger from "../../utils/logger/logger";
 
 export default class ReadyEvent {
@@ -7,9 +7,24 @@ export default class ReadyEvent {
 
   execute(client: Client) {
     logger.startup(`Logged in as ${client.user?.username}`);
-    client.user?.setActivity({
-      name: "Celestia",
-      type: ActivityType.Playing,
-    });
+
+    const updateActivity = async () => {
+      try {
+        const response = await fetch("http://34.150.153.214:8080/clients");
+        const data = await response.json();
+        const clients = data.amount;
+
+        client.user?.setActivity({
+          name: `${clients} Players`,
+          type: ActivityType.Watching,
+        });
+      } catch (error) {
+        logger.error("Failed to fetch client count:", error);
+      }
+    };
+
+    updateActivity();
+
+    setInterval(updateActivity, 60000);
   }
 }
