c8c9415e176b5a9f6b454e15b34048dc48bd27f2
Itztiva <tiva@itztiva.com>
2024-07-27 00:12:41 +0000

    fixes

--- src/routes/celestia/updater.ts
+++ src/routes/celestia/updater.ts
@@ -3,10 +3,10 @@ import app from '../../'
 export default function () {
     app.get("/api/launcher/updater/:version", async (c) => {
         return c.json({
-            "version": "1.0.0",
+            "version": "1.0.1",
             "pub_date": "2020-09-18T12:29:53+01:00",
-            "url": "https://cdn.celestiafn.com/Celestia_1.0.0_x64_en-US.msi.zip",
-            "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRNWxLVUZ2dERDL1htVWxQcjlIZWRMS3RrYXhOcnlyOFhrRlozQ2I3M2h6ZjRmNzR2NUFESmpGcDBYK0N1NytpZ2FGa0twQWtTNStqZDZVN0llTzJad1pwY3BlVit5endnPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzIyMDEwNjY1CWZpbGU6Q2VsZXN0aWFfMS4wLjBfeDY0X2VuLVVTLm1zaS56aXAKNEZ3N2JlNVl0RXdmdWtGbnNXVGVocUl6NGZBaEJ2MzNEYUFnR0R1dmQ3UjhYSnR5L1lOSGZsQy9ZNjlIRndyV2pYY3FwSDhBdTJJQU9yOFppdWRwQ3c9PQo=",
+            "url": "https://cdn.celestiafn.com/Celestia_1.0.1_x64_en-US.msi.zip",
+            "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRNWxLVUZ2dERDL1ZFWDJlREZnRmpTRURvdzBveGV4eW5FdFphaDlYYldVeVlia0kwSUZXdkVFWkQ1RGNqenJsVTl6cGJVc0ZnVThobGZBWW1WTWFCMjcvbjRoSE80anc0PQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzIyMDM2NTczCWZpbGU6Q2VsZXN0aWFfMS4wLjFfeDY0X2VuLVVTLm1zaS56aXAKejJMZ2cxY3lZYVpZNUkyc3phRzNHdk1LbmFGNjZtQmV1bjdFVVZwa3FyVHp1VVZLVUg2ckVWZENlNm9pUkFnUmoxOVppdCtCSmRuTUI0M0laYXdBQ1E9PQo=",
             "notes": null
           })
     })
--- src/routes/client/lightswitch.ts
+++ src/routes/client/lightswitch.ts
@@ -12,25 +12,6 @@ export default function () {
 
     const user = await User.findOne({ accountId: token.accountId })
 
-    if (user?.banned == true ) {
-      return c.json([
-        {
-          serviceInstanceId: "fortnite",
-          status: "DOWN",
-          message: "fortnite is DOWN.",
-          maintenanceUri: null,
-          overrideCatalogIds: ["a7f138b2e51945ffbfdacc1af0541053"],
-          allowedActions: [],
-          banned: true,
-          launcherInfoDTO: {
-            appName: "Fortnite",
-            catalogItemId: "4fe75bbc5a674f4f9b356b5c90567da5",
-            namespace: "fn",
-          },
-        },
-      ]);
-    }
-    
     return c.json({
       serviceInstanceId: "fortnite",
       status: "UP",
@@ -54,25 +35,6 @@ export default function () {
     if (!token) return c.json(Solara.authentication.invalidHeader, 400)
 
     const user = await User.findOne({ accountId: token.accountId })
-
-    if (user?.banned == true) {
-      return c.json([
-        {
-          serviceInstanceId: "fortnite",
-          status: "DOWN",
-          message: "fortnite is DOWN.",
-          maintenanceUri: null,
-          overrideCatalogIds: ["a7f138b2e51945ffbfdacc1af0541053"],
-          allowedActions: [],
-          banned: true,
-          launcherInfoDTO: {
-            appName: "Fortnite",
-            catalogItemId: "4fe75bbc5a674f4f9b356b5c90567da5",
-            namespace: "fn",
-          },
-        },
-      ]);
-    }
 
     return c.json([
       {
